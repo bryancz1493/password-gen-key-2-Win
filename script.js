7f1740a3249abfcd47a4ea066e1b4e0305a96590
bryancz1493 <bryancz1493@gmail.com>
2023-10-05 00:15:15 -0700

    Assign for loop to generate password in the range of (8< i < 128) and at least one character type is picked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-function writePassword (length, useLowerCase, useUpperCase, useNumberic, useSpecialCharacters) {
+function writePassword (minLength, maxLength, useLowerCase, useUpperCase, useNumberic, useSpecialCharacters) {
     var LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
     var UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var NumbericCharacters = "1234567890";
@@ -18,24 +18,45 @@ function writePassword (length, useLowerCase, useUpperCase, useNumberic, useSpec
     if (charset === "useSpecialCharacters") {
         charset += SpecialCharacters;
     }
-    if (!charset) {
-        return;
+    if (charset.length !== 0) {
+        throw new Error ("Must pick at least one character type to proceed");
     }
+
+    var passwordLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength;
+    for (i =0; i < passwordLength; i++) {
+        var randomPass = Math.floor(Math.random() * charset.length);
+        password += charset[randomPass];
+    }
+    
+    return password;
 }
+
+// Add prompt for each character type
+var minLength = 8;
+var maxLength = 128;
+var useLowerCase = confirm("Include lowercase characters?");
+var useUpperCase = confirm("Include uppercase characters?");
+var useNumberic = confirm("Include numberic characters?");
+var useSpecialCharacters = confirm("Include special characters?");
+
+var password = generatePassword(minLength, maxLength, useLowerCase, useUpperCase, useNumberic, useSpecialCharacters);
+var passwordText = document.querySelector("#password");
+console.log(password);
+
+generateBtn.addEventListener("click", writePassword);
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
   var passwordText.value = password;
 
-}
-
-// Add prompt for each character type
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
