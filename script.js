f49a83e81b43e783c2759cf6ed907b522aaefd87
bryancz1493 <bryancz1493@gmail.com>
2023-10-04 23:32:27 -0700

    Declare the 4 character types and assign prompt accordingly

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+function writePassword (length, useLowerCase, useUpperCase, useNumberic, useSpecialCharacters) {
+    var LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    var UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var NumbericCharacters = "1234567890";
+    var SpecialCharacters = "!@#$%^&*()_-+=";
 
+    let charset = "";
+    if (charset === "useLowerCase") {
+        charset += LowerCaseCharacters;
+    }
+    if (charset === "useUpperCase") {
+        charset += UpperCaseCharacters;
+    }
+    if (charset === "useNumberic") {
+        charset += NumbericCharacters;
+    }
+    if (charset === "useSpecialCharacters") {
+        charset += SpecialCharacters;
+    }
+    if (!charset) {
+        return;
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,14 +29,13 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#abcqefghijklmnopqrstuvwxyz");
-  function generatePassword(length) {
-    var password = "#password"
-  }
+  var passwordText = document.querySelector("#password");
 
   var passwordText.value = password;
 
 }
+
+// Add prompt for each character type
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
